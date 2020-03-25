7ba4f290d354ab567949aa167ae169054bded304
hassan.adel <hasanadel555@hotrmail.com>
2020-03-25 21:02:15 +0200

    IoC using setter

--- src/com/tutorial/TaxReport.java
+++ src/com/tutorial/TaxReport.java
@@ -3,11 +3,17 @@ package com.tutorial;
 public class TaxReport {
     private TaxCalculator calculator;
 
-    public TaxReport(TaxCalculator calculator) {
-        this.calculator = calculator;
+    public TaxReport() {
     }
+
     public void show(){
         var tax = calculator.calculateTaxes();
         System.out.println(tax);
     }
+
+    public TaxReport setCalculator(TaxCalculator calculator) {
+        this.calculator = calculator;
+        return this;
+    }
 }
+
